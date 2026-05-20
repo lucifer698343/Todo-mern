621e5e2b3fb6a14b85faceb9a25057406021b6a8
lucifer698343 <k.heroayus777@gmail.com>
2026-05-20 23:25:36 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,35 @@ const adminRoutes = require('./routes/adminRoutes');
 const errorMiddleware=require('./middleware/errorMiddleware')
 const cors = require('cors');
 const path = require('path');
+
 const app=express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://todo-mern-frontend-4mkv.onrender.com',
+    credentials: true
+}));
 
 //connect db
 connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
 
 //routes
 app.use('/auth',authRoutes);
 app.use('/todo',todoRoutes);
 app.use('/admin', adminRoutes);
 
+//uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 //error middleware
 app.use(errorMiddleware);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 //starting the server
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT}`)
