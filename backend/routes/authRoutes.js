0af6805b91b7d7136e15df5ff0c7f5584d1f2024
lucifer698343 <k.heroayus777@gmail.com>
2026-05-25 16:11:12 +0545

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,17 @@ const express=require('express');
 const router=express.Router();
 const middleware=require('../middleware/authmiddleware')
 const upload = require('../middleware/uploadMiddleware');
-const {body}=require('express-validator')
+const{
+    registerValidation,
+    loginValidation
+}=require('../validators/authValidator')
+
+
 
 
 const{
     registerUser,
+    googleAuth,
     userLogin,
     Profile,
     uploadProfileImage
@@ -14,34 +20,16 @@ const{
 
 
 //register a new user account
-router.post('/Registration',
-    [
-        body('name')
-        .notEmpty().withMessage("your name cannot be empty"),
-
-        body('email')
-        .notEmpty().withMessage("email cannot be empty")
-        .isEmail().withMessage("invalid email format"),
-
-        body('password')
-        .notEmpty().withMessage("password cannot be empty")
-        .isLength({min:6, max:12})
-
-    ],
-    registerUser
+router.post('/Registration',registerValidation,registerUser
 
 )
-
+//login with google
+router.post(
+    '/google-auth',
+    googleAuth
+);
 //login to user acccount
-router.post('/login',
-    [
-         body('email')
-        .isEmail().withMessage('Invalid email'),
-
-        body('password')
-        .notEmpty().withMessage('Password required')
-    ],
-    userLogin)
+router.post('/login',loginValidation,userLogin)
 
 //middleware for protected routes
 router.use(middleware);
