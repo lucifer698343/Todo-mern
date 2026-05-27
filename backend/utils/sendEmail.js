5fd0dd3ade75aaa4de7c56898ef0e09a8282800c
lucifer698343 <k.heroayus777@gmail.com>
2026-05-27 14:38:21 +0545

    Updated my code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4
 });
 
 const sendEmail = async (email, otp) => {
